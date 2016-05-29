aa8302d893732c689929f56673c9eebdbadf44a8
Michael Ciniawsky <michael.ciniawsky@gmail.com>
2016-05-30 00:15:30 +0200

    lib(): update package

--- index.js
+++ index.js
@@ -1,3 +1,33 @@
 // ------------------------------------
 // #POSTCSS - LOAD OPTIONS
 // ------------------------------------
+
+'use strict'
+
+const path = require('path')
+const config = require('cosmiconfig')
+
+module.exports = function (options) {
+  function loadOptions (config) {
+    if (typeof options === 'string') {
+      options = require(path.join(process.cwd(), options))
+    }
+
+    if (typeof options === 'object') {
+      for (let option in options) {
+        config[option] = options[option]
+      }
+    }
+
+    delete config.plugins
+
+    options = config
+
+    return options
+  }
+
+  return config('postcss')
+    .catch((error) => console.log(error))
+    .then((result) => loadOptions(result.config))
+    .then((options) => console.log(options))
+}
