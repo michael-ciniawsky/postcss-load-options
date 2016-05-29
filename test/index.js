d436400fb50740733643ca32a96e7a185ac81b5f
Michael Ciniawsky <michael.ciniawsky@gmail.com>
2016-05-30 00:15:12 +0200

    test(): update tests

--- test/index.js
+++ test/index.js
@@ -3,10 +3,12 @@
 const fs = require('fs')
 
 const postcss = require('postcss')
-const plugins = require('../index')
+const options = require('../index')()
 
 const css = fs.readFileSync('./fixtures/index.css', 'utf8')
 
-postcss(plugins())
-  .process(css)
-  .then(result => console.log(result.css))
+console.log(options)
+
+// postcss([])
+//   .process(css, options)
+//   .then(result => console.log(result.css))
--- test/postcss.config.js
+++ test/postcss.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  parser: 'sugarss',
+  parser: 'postcss-scss',
   plugins: {
-    bem: {
+    'postcss-bem': {
       defaultNamespace: 'undefined',
       style: 'bem',
       separators: {
