3e5fd9b3ec117a88e050a644d5f6c14d3fcdc126
Michael Ciniawsky <michael.ciniawsky@gmail.com>
2016-05-31 21:08:29 +0200

    lib(): update package

--- index.js
+++ index.js
@@ -4,30 +4,12 @@
 
 'use strict'
 
-const path = require('path')
 const config = require('cosmiconfig')
 
+const loadOptions = require('./lib/loadOptions')
+
 module.exports = function (options) {
-  function loadOptions (config) {
-    if (typeof options === 'string') {
-      options = require(path.join(process.cwd(), options))
-    }
-
-    if (typeof options === 'object') {
-      for (let option in options) {
-        config[option] = options[option]
-      }
-    }
-
-    delete config.plugins
-
-    options = config
-
-    return options
-  }
-
   return config('postcss')
     .catch((error) => console.log(error))
-    .then((result) => loadOptions(result.config))
-    .then((options) => console.log(options))
+    .then((result) => loadOptions(result.config, options))
 }
