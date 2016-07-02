cc421504c8f3dc433d12fb84e844c9988f9f8e5b
Michael Ciniawsky <michael.ciniawsky@gmail.com>
2016-07-02 05:11:12 +0200

    test(): rm old, add new tests

--- test/index.js
+++ test/index.js
@@ -6,63 +6,55 @@
 
 const test = require('ava')
 
-const postcss = require('postcss')
-const optionsrc = require('../index')
-
+const { join } = require('path')
 const { readFileSync, writeFileSync } = require('fs')
 
-function readSync (path) {
-  return readFileSync(path, 'utf8')
-}
+const fixtures = (file) => readFileSync(join(__dirname, 'fixtures', file))
+const expected = (file) => readFileSync(join(__dirname, 'expects', file))
 
-function writeSync (path, file) {
-  return writeFileSync(path, file, 'utf8')
-}
+const postcss = require('postcss')
+const optionsrc = require('../')
 
-test('Process SSS with default options', (t) => {
+test('1 - Process SSS with default options', (t) => {
   optionsrc().then((options) => {
     postcss([])
-      .process(readSync('./fixtures/index.sss'), options)
+      .process(fixtures('index.sss'), options)
       .then(result => {
-        writeSync('./expect/index.sss.css', result.css)
-        t.equal(result.css, readSync('./expect/index.sss.css'))
-        writeSync('./results/index.sss.css', result.css)
+        writeFileSync('./expects/index.sss.css', result.css)
+        t.is(expected('./expect/index.sss.css'), result.css)
       })
   })
 })
 
-test('Process SCSS with custom options', (t) => {
+test('2 - Process SCSS with custom options', (t) => {
   optionsrc('postcssrc.json').then((options) => {
     postcss([])
-      .process(readSync('./fixtures/index.scss'), options)
+      .process(fixtures('index.scss'), options)
       .then(result => {
-        writeSync('./expect/index.scss.css', result.css)
-        t.equal(result.css, readSync('./expect/index.scss.css'))
-        writeSync('./results/index.scss.css', result.css)
+        writeFileSync('./expects/index.scss.css', result.css)
+        t.is(expected('index.scss.css'), result.css)
       })
   })
 })
 
-test('Process LESS with custom options', (t) => {
+test('3 - Process LESS with custom options', (t) => {
   optionsrc('postcss.config.js').then((options) => {
     postcss([])
-      .process(readSync('./fixtures/index.less'), options)
+      .process(fixtures('index.less'), options)
       .then(result => {
-        writeSync('./expect/index.less.css', result.css)
-        t.equal(result.css, readSync('./expect/index.less.css'))
-        writeSync('./results/index.less.css', result.css)
+        writeFileSync('./expects/index.less.css', result.css)
+        t.is(expected('index.less.css'))
       })
   })
 })
 
-// test('Process CSS-in-JS with custom options', (t) => {
-//   optionsrc('postcss.custom.js').then((options) => {
-//     postcss([])
-//       .process(readSync('./fixtures/index.js'), options)
-//       .then(result => {
-//         writeSync('./expect/index.js.css', result.css)
-//         t.equal(result.css, readSync('./expect/index.js.css'))
-//         writeSync('./results/index.js.css', result.css)
-//       })
-//   })
-// })
+test.skip('4 - Process CSS-in-JS with custom options', (t) => {
+  optionsrc('postcss.custom.js').then((options) => {
+    postcss([])
+      .process(fixtures('index.js'), options)
+      .then(result => {
+        writeFileSync('./expects/index.js.css', result.css)
+        t.is(expected('index.js.css'))
+      })
+  })
+})
--- test/postcss.custom.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  parser: 'postcss-js',
-  map: 'inline',
-  from: './fixtures/index.js',
-  to: './results/index.js.css'
-}
