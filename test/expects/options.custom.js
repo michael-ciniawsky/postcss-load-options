1fe55ea62ef7d311885a5dda96f06afd68ba2433
Michael Ciniawsky <michael.ciniawsky@gmail.com>
2016-10-10 03:01:11 +0200

    test(): cleanup

--- test/expects/options.custom.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  parser: require('postcss-scss'),
-  map: 'inline',
-  from: './fixtures/index.css',
-  to: './expects/custom.css'
-}
--- test/expects/options.default.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  parser: require('sugarss'),
-  map: 'inline',
-  from: './fixtures/index.sss',
-  to: './expects/index.css'
-}
